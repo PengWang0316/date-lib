3d34482ad8c7a5cbcfa5ff7dd284744ac4dd8ee7
Kevin W <rocwang@cityuniversity.edu>
2018-03-19 19:49:55 -0700

    Add test code for the getDateString method.

--- __tests__/index.js
+++ __tests__/index.js
@@ -19,4 +19,19 @@ describe('DateLib test', () => {
     expect(DateLib.getCurrentDateString('mm-dd-yyyy')).toBe('11-06-2018');
     clock.restore();
   });
+
+  test('getDateString', () => {
+    expect(DateLib.getDataString('01/01/2018', 'yyyy/mm/dd')).toBe('2018/01/01');
+    expect(DateLib.getDataString('02/03/2018', 'yyyy/mm/dd')).toBe('2018/02/03');
+    expect(DateLib.getDataString('01-01-2018', 'yyyy/mm/dd')).toBe('2018/01/01');
+    expect(DateLib.getDataString('02/03/2018', 'yyyy-mm-dd')).toBe('2018-02-03');
+    expect(DateLib.getDataString('2018/03/16', 'mm-dd-yyyy')).toBe('03-16-2018');
+    expect(DateLib.getDataString(new Date('2018/03/16'), 'mm-dd-yyyy')).toBe('03-16-2018');
+    expect(DateLib.getDataString(new Date('2018/03/16'), 'mm/dd/yyyy')).toBe('03/16/2018');
+    expect(DateLib.getDataString(new Date('01-02-2018'), 'yyyy/mm/dd')).toBe('2018/01/02');
+    expect(() => DateLib.getDataString(1, 'yyyy/mm/dd')).toThrowError('Please offer a date string or Date object for the param.');
+    expect(() => DateLib.getDataString({}, 'yyyy/mm/dd')).toThrowError('Please offer a date string or Date object for the param.');
+    expect(() => DateLib.getDataString([], 'yyyy/mm/dd')).toThrowError('Please offer a date string or Date object for the param.');
+    expect(() => DateLib.getDataString(true, 'yyyy/mm/dd')).toThrowError('Please offer a date string or Date object for the param.');
+  });
 });
